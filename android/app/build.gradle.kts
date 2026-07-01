8c73f191a804bf11c1f59b4b27be82417f4c0c9b
waziri1977 <ibnhussan@gmail.com>
2026-07-01 12:37:06 +0100

    Initial Flutter commit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,12 +1,20 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
 android {
-    namespace = "com.example.new_disciples"
+    namespace = "com.emirateexpress.disciples"
     compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
@@ -20,18 +28,27 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.new_disciples"
+        applicationId = "com.emirateexpress.disciples"
         minSdk = flutter.minSdkVersion
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.2"
+    }
+
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+        }
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 }
--- android/app/src/main/kotlin/com/emirateexpress/disciples/MainActivity.kt
+++ android/app/src/main/kotlin/com/emirateexpress/disciples/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.new_disciples
+package com.emirateexpress.disciples
 
 import io.flutter.embedding.android.FlutterActivity
 
